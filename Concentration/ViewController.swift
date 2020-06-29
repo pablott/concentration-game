3b957ff53d76059954c296126bdff019a9cfccf9
Pablo Trabajos <hz302783@hertz.net>
2020-06-29 12:48:26 +0100

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Pablo Tamayo on 29/06/2020.
+//  Copyright © 2020 Pablo Tamayo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
